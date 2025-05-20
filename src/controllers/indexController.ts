a0e09b6ef2f3b3d69809a6b89a6b11388d2eaf91
Rimmert Zelle <rimmert.zelle@gmail.com>
2025-05-20 22:17:22 +0200

    deleted some console.logs

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -5,10 +5,8 @@ Dotenv.config({ path: '.env' });
 import { getData } from '../utils/ajax.js';
 
 const apiUrl: string = process.env.API_URL;
-console.warn('🐮', apiUrl);
 
 export const getIndex = async (req: Request, res: Response): Promise<void> => {
   const article: ArticleResponse = await getData('http://localhost:3012/articles/1');
-  console.log('🐮', article);
   res.render('index', { article: article });
 };
